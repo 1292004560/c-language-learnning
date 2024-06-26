694a69cf37c7866a434943e2a57273615d9ea03e
zhoushuiping <zhou-shui-ping@outlook.com>
2024-06-26 19:59:22 +0800

    sylar

---
+++ sylar/sylar/log.h
@@ -0,0 +1,26 @@
+#ifndef __SYLAR_LOG_H__
+#define __SYLAR_LOG_H__
+
+#include <string>
+#include <stdint.h>
+#include <memory>
+#include <list>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <stdarg.h>
+#include <map>
+
+namespace sylar {
+
+
+
+  class Logger;
+class LoggerManager;
+
+
+}
+
+
+
+#endif
